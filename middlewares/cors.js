f5734c3d6b235e99a95f693a5167519093b8a367
antip4d <timtim0250@inbox.ru>
2024-05-20 19:24:07 +0300

    siiuu2.0

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,17 @@
 
-const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'localhost:3000'
-];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ['https://antip4d.nomoredomainswork.ru/'];
 
 function cors(req, res, next) {
     const { origin } = req.headers;
 
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    if (allowedCors.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
     }
-
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 }
 
-module.exports = {
-    cors
-};
+module.exports = cors;
